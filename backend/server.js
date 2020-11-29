d592f60963ce31a46fc4d03399683600b0d0d9f8
lucas-utd <lucaschentao@gmail.com>
2020-11-29 10:09:22 -0600

    Create sample users in MongoDB Atlas.

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,26 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 import data from "./data.js";
 import morgan from "morgan";
+import userRouter from "./routers/userRouter.js";
+
+dotenv.config();
 
 const app = express();
 
 app.use(morgan("dev"));
+
+const uri = process.env.REACT_APP_ATLAS_URI;
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully");
+});
 
 app.get("/api/products/:id", (req, res) => {
   const product = data.products.find(
@@ -21,8 +37,14 @@ app.get("/api/products", (req, res) => {
   res.json(data.products);
 });
 
+app.use("/api/users", userRouter);
+
 app.get("/", (req, res) => {
   res.json("Server is ready");
+});
+
+app.use((err, req, res) => {
+  res.status(500).json({ message: err.message });
 });
 
 const port = process.env.PORT || 5000;
