0d8e58da7b6c5d898ad3594bf9c3270830a090f6
lucas-utd <lucaschentao@gmail.com>
2020-11-28 22:52:00 -0600

    Implement logical about add to cart.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,31 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { BrowserRouter, Link, Route } from "react-router-dom";
 import CartView from "./views/CartView";
 import HomeView from "./views/HomeView";
 import ProductView from "./views/ProductView";
 
 function App() {
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+
   return (
     <BrowserRouter>
       <div className="grid-container">
         <header className="row">
           <div>
-            <a className="brand" href="/">
+            <Link className="brand" to="/">
               Bicycles Shop
-            </a>
+            </Link>
           </div>
           <div>
-            <a href="/cart">Cart</a>
-            <a href="/signin">Sign In</a>
+            <Link to="/cart">
+              Cart
+              {cartItems.length > 0 && (
+                <span className="badge">{cartItems.length}</span>
+              )}
+            </Link>
+            <Link to="/signin">Sign In</Link>
           </div>
         </header>
         <main>
