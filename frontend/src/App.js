60d5a8636aba6542ffa3f6faf399dd2f1b2e343a
lucas-utd <lucaschentao@gmail.com>
2020-12-09 10:03:28 -0600

    Create SigninView interface.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Link, Route } from "react-router-dom";
 import CartView from "./views/CartView";
 import HomeView from "./views/HomeView";
 import ProductView from "./views/ProductView";
+import SigninView from "./views/SigninView";
 
 function App() {
   const cart = useSelector((state) => state.cart);
@@ -31,6 +32,7 @@ function App() {
         <main>
           <Route path="/cart/:id?" component={CartView}></Route>
           <Route path="/product/:id" component={ProductView}></Route>
+          <Route path="/signin" component={SigninView}></Route>
           <Route path="/" component={HomeView} exact></Route>
         </main>
         <footer className="row center">All right reserved</footer>
